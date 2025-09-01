c722810c51e1c8816a7c18fe3b8d0b343194821a
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2025-09-01 13:00:51 -0600

    instalacion de plantilla

---
+++ resources/views/partials/app/footer.blade.php
@@ -0,0 +1,7 @@
+<!--start footer-->
+<footer class="footer">
+    <div class="footer-text">
+    Copyright © {{ date('Y') }}. NOVA - Capacitación todos los derechos reservados.
+    </div>
+</footer>
+<!--end footer-->
---
+++ resources/views/partials/app/javascript.blade.php
@@ -0,0 +1,14 @@
+<!-- JS Files-->
+<script src="/template/assets/js/jquery.min.js"></script>
+<script src="/template/assets/plugins/simplebar/js/simplebar.min.js"></script>
+<script src="/template/assets/plugins/metismenu/js/metisMenu.min.js"></script>
+<script src="/template/assets/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<!--plugins-->
+<script src="/template/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+<script src="/template/assets/plugins/apexcharts-bundle/js/apexcharts.min.js"></script>
+<script src="/template/assets/plugins/easyPieChart/jquery.easypiechart.js"></script>
+<script src="/template/assets/plugins/chartjs/chart.min.js"></script>
+<script src="/template/assets/js/index.js"></script>
+<!-- Main JS-->
+<script src="/template/assets/js/main.js"></script>
